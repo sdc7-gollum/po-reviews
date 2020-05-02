17ce19aa2f2d7a68d13c439bca1a16b965e71a13
Eric Song <eric.song@columbia.edu>
2020-05-01 17:16:02 -0700

    Implement seeder.js and refactor database schema

--- database/index.js
+++ database/index.js
@@ -3,20 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const PlaceSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
-});
-
-const ReviewSchema = new Schema({
-  place: { type: Schema.Types.ObjectId, ref: 'Place' },
-  username: String,
-  pic: String,
-  date: Date,
-  body: String,
-  ratings: [Number],
+  reviews: Array,
 });
 
 const Place = mongoose.model('Place', PlaceSchema);
-const Review = mongoose.model('Review', ReviewSchema);
 
-module.exports = { Place, Review };
+module.exports.Place = Place;
---
+++ database/seeder.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const faker = require('faker');
+const db = require('./index');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/po-reviews')
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err));
+
+const generateReviews = () => {
+  const reviews = [];
+  const numReviews = Math.floor(Math.random() * 470 + 30);
+  const getRandomRating = () => Math.floor(Math.random() * 5 + 1);
+  for (let i = 0; i < numReviews; i += 1) {
+    const review = {
+      name: faker.name.firstName(),
+      pic: faker.image.people(),
+      date: faker.date.recent(),
+      body: faker.lorem.paragraph(),
+      r_checkin: getRandomRating(),
+      r_accuracy: getRandomRating(),
+      r_location: getRandomRating(),
+      r_communication: getRandomRating(),
+      r_cleanliness: getRandomRating(),
+      r_value: getRandomRating(),
+    };
+    reviews.push(review);
+  }
+  return reviews;
+};
+
+for (let i = 0; i < 100; i += 1) {
+  db.Place.collection.insertOne({ reviews: generateReviews() });
+}
