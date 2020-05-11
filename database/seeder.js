d2f08ce2b419b1becb8628f65d524fb22c9984cd
Eric Song <eric.song@columbia.edu>
2020-05-11 23:32:50 +0900

    Refactor random generator algorithm

--- database/seeder.js
+++ database/seeder.js
@@ -1,16 +1,22 @@
 const faker = require('faker');
+const imageOptions = require('./imageOptions');
 const db = require('./index');
 
 const generateReviews = () => {
   const reviews = [];
   const numReviews = Math.floor(Math.random() * 470 + 30);
-  const getRandomRating = () => Math.floor(Math.random() * 5 + 1);
+  // originally implemented method below via Math.floor(Math.random() * 5 + 1),
+  // but refactored to better reflect reality
+  const getRandomRating = () => {
+    const options = [5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 2, 2, 1];
+    return options[Math.floor(Math.random() * 15)];
+  };
   for (let i = 0; i < numReviews; i += 1) {
     const review = {
       name: faker.name.firstName(),
-      pic: faker.image.people(),
-      date: faker.date.recent(),
-      body: faker.lorem.paragraph(),
+      pic: imageOptions[Math.floor(Math.random() * 40)],
+      date: faker.date.between('2019-01-01', '2020-05-05'),
+      body: faker.lorem.paragraphs(),
       r_checkin: getRandomRating(),
       r_accuracy: getRandomRating(),
       r_location: getRandomRating(),
