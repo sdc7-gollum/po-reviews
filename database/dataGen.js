5b93ef0eb9d9583c6cd873cb45b8bd63b2ce94c8
Zechariah Deckert-Holscher <zdeckert@gmail.com>
2020-05-22 11:46:05 -0700

    created .csv and dataGen.js

---
+++ database/dataGen.js
@@ -0,0 +1,33 @@
+const faker = require('faker');
+const imageOptions = require('./imageOptions');
+
+const generateReviews = () => {
+  const reviews = [];
+  const numReviews = Math.floor(Math.random() * 40 + 30);
+  const getRandomRating = () => {
+    const options = [5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 2, 1];
+    return options[Math.floor(Math.random() * 12)];
+  };
+  for (let i = 0; i < numReviews; i += 1) {
+    const review = {
+      name: faker.name.firstName(),
+      pic: imageOptions[Math.floor(Math.random() * 40)],
+      date: faker.date.between('2019-01-01', '2020-05-05'),
+      body: faker.lorem.paragraphs(),
+      r_checkin: getRandomRating(),
+      r_accuracy: getRandomRating(),
+      r_location: getRandomRating(),
+      r_communication: getRandomRating(),
+      r_cleanliness: getRandomRating(),
+      r_value: getRandomRating(),
+    };
+    reviews.push(review);
+  }
+  return reviews;
+};
+
+const data = [];
+
+for (let i = 0; i < 100; i += 1) {
+  data.push({ id: i + 1, reviews: generateReviews() });
+}
