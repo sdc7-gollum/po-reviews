9bcb8b4ef6616e891dbf0b503c9777440d90d45c
Zechariah Deckert-Holscher <zdeckert@gmail.com>
2020-05-22 15:34:54 -0700

    initial csv generation

--- database/dataGen.js
+++ database/dataGen.js
@@ -1,33 +1,52 @@
 const faker = require('faker');
+const fs = require('fs');
 const imageOptions = require('./imageOptions');
 
-const generateReviews = () => {
-  const reviews = [];
-  const numReviews = Math.floor(Math.random() * 40 + 30);
-  const getRandomRating = () => {
-    const options = [5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 2, 1];
-    return options[Math.floor(Math.random() * 12)];
-  };
-  for (let i = 0; i < numReviews; i += 1) {
-    const review = {
-      name: faker.name.firstName(),
-      pic: imageOptions[Math.floor(Math.random() * 40)],
-      date: faker.date.between('2019-01-01', '2020-05-05'),
-      body: faker.lorem.paragraphs(),
-      r_checkin: getRandomRating(),
-      r_accuracy: getRandomRating(),
-      r_location: getRandomRating(),
-      r_communication: getRandomRating(),
-      r_cleanliness: getRandomRating(),
-      r_value: getRandomRating(),
-    };
-    reviews.push(review);
+
+const writeReviews = fs.createWriteStream('reviews.csv');
+writeReviews.write('id,name,pic,date,body,r_checking,r_accuracy,r_location,r_communication,r_cleanliness,r_value\n', 'utf8');
+
+
+function writeTenMillionReviews(writer, encoding, callback) {
+  let i = 10000000;
+  let id = 0;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const numReviews = Math.floor(Math.random() * 3 + 3);
+      const getRandomRating = () => {
+        const options = [5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 2, 1];
+        return options[Math.floor(Math.random() * 12)];
+      };
+      for (let r = 0; r < numReviews; r += 1) {
+        const name = faker.name.firstName();
+        const pic = imageOptions[Math.floor(Math.random() * 40)];
+        const body = faker.lorem.paragraphs();
+        const rCheckin = getRandomRating();
+        const rAccuracy = getRandomRating();
+        const rLocation = getRandomRating();
+        const rCommunicatiom = getRandomRating();
+        const rCleanliness = getRandomRating();
+        const rValue = getRandomRating();
+        const data = `${id},${name},${pic},${body},${rCheckin},${rAccuracy},${rLocation},${rCommunicatiom},${rCleanliness},${rValue}\n`;
+        if (i === 0) {
+          writer.write(data, encoding, callback);
+        } else {
+          ok = writer.write(data, encoding);
+        }
+      }
+      console.log(id);
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
   }
-  return reviews;
-};
-
-const data = [];
-
-for (let i = 0; i < 100; i += 1) {
-  data.push({ id: i + 1, reviews: generateReviews() });
+  write();
 }
+
+writeTenMillionReviews(writeReviews, 'utf-8', () => {
+  writeReviews.end();
+  console.log('csv built');
+});
